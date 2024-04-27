5e0171bc7f4b7e4fa98ed347a24da2147a6017bb
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2024-04-27 11:40:03 +0530

    initialized repo

---
+++ src/main/java/com/mutualfunds/backend/mutualfundapi/MutualfundapiApplication.java
@@ -0,0 +1,14 @@
+package com.mutualfunds.backend.mutualfundapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualfundapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualfundapiApplication.class, args);
+		System.out.println("hello World");
+	}
+
+}
