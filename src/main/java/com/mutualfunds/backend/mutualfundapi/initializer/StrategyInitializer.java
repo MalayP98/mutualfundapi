0094124d9f2d7a98be3a7dc60e907130005cb6c4
MalayP98 <tomalaypandey.tanu@gmail.com>
2024-04-27 15:13:10 +0530

    added exception if initilizing failed

--- src/main/java/com/mutualfunds/backend/mutualfundapi/initializer/StrategyInitializer.java
+++ src/main/java/com/mutualfunds/backend/mutualfundapi/initializer/StrategyInitializer.java
@@ -3,22 +3,32 @@ package com.mutualfunds.backend.mutualfundapi.initializer;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.mutualfunds.backend.mutualfundapi.constants.JsonConstants;
 import com.mutualfunds.backend.mutualfundapi.pojo.entity.FundStrategy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Slf4j
 public class StrategyInitializer implements CommandLineRunner {
 
-    private final static String STRATEGIES_PATH = "src/main/resources/jsons/strategies.json";
+    private final static String STRATEGIES_PATH = "src/main/resources/jsons/";
+
+    private final static String FILE_NAME = "strategies.json";
 
     @Override
     public void run(String... args) throws Exception {
-        List<FundStrategy> strategies = JsonConstants.OBJECT_MAPPER.readValue(
-                new File(STRATEGIES_PATH),
-                new TypeReference<List<FundStrategy>>() {}
-        );
+        List<FundStrategy> strategies = new ArrayList<>();
+        try{
+            strategies = JsonConstants.OBJECT_MAPPER.readValue(
+                    new File(STRATEGIES_PATH + FILE_NAME),
+                    new TypeReference<List<FundStrategy>>() {}
+            );
+        }catch (Exception e){
+            log.error("Error while reading file '{}'. Message : {}", FILE_NAME, e.getMessage());
+        }
     }
 }
