115c1282538cdb05d3b8b92353d6590f821a17d2
MalayP98 <tomalaypandey.tanu@gmail.com>
2024-04-28 16:22:59 +0530

    minor bug fixes

--- src/main/java/com/mutualfunds/backend/mutualfundapi/resource/PaymentResource.java
+++ src/main/java/com/mutualfunds/backend/mutualfundapi/resource/PaymentResource.java
@@ -1,5 +1,7 @@
 package com.mutualfunds.backend.mutualfundapi.resource;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mutualfunds.backend.mutualfundapi.constants.JsonConstants;
 import com.mutualfunds.backend.mutualfundapi.daos.PaymentDAO;
 import com.mutualfunds.backend.mutualfundapi.dto.PaymentResponseDTO;
 import com.mutualfunds.backend.mutualfundapi.services.PaymentService;
@@ -18,4 +20,9 @@ public class PaymentResource {
     public ResponseEntity<PaymentResponseDTO> getPaymentLink(@RequestBody PaymentDAO paymentInfo){
         return ResponseEntity.ok(paymentService.getPaymentLink(paymentInfo));
     }
+
+//    public static void main(String[] args) throws JsonProcessingException {
+//        PaymentDAO paymentDAO = new PaymentDAO();
+//        System.out.println(JsonConstants.OBJECT_MAPPER.writeValueAsString(paymentDAO));
+//    }
 }
