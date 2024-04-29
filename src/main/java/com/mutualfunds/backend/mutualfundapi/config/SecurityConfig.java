7d7ff76e8d69656dd1c4268eda12ce56aab6aee1
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2024-04-29 09:46:14 +0530

    Fixed 403 error on every alternate call

--- src/main/java/com/mutualfunds/backend/mutualfundapi/config/SecurityConfig.java
+++ src/main/java/com/mutualfunds/backend/mutualfundapi/config/SecurityConfig.java
@@ -3,15 +3,21 @@ package com.mutualfunds.backend.mutualfundapi.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+        // http.cors();
+        // http.csrf().disable();
+        // http.headers().frameOptions().disable();
+        // http.authorizeRequests().anyRequest().permitAll();
+
+        http.cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().permitAll().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
